f3ee1b54921c539820a822380010e5450cb57a37
agnedzemydaite <agne.dzemydaite@mif.stud.vu.lt>
2025-12-19 21:41:21 +0200

    Add files via upload
    url funkcijos kad sukurti tld sarasa ir url israiska.

---
+++ url_funkcijos.cpp
@@ -0,0 +1,33 @@
+#include "url_funkcijos.h"
+
+using std::vector;
+using std::wifstream;
+using std::wstring;
+using std::wregex;
+using std::regex_constants::icase;
+
+wstring tld_saraso_sudarymas(){
+    vector <wstring> tld;
+    wifstream fin("url_visos.txt");
+    wstring a;
+    while (fin >> a) {
+        tld.push_back(a);
+    }
+    fin.close();
+    wstring tld_sablonas = L"(";
+    for (size_t i = 0; i < tld.size(); ++i) {
+        tld_sablonas += tld[i];
+        if (i != tld.size() - 1) tld_sablonas += L"|";
+    }
+    tld_sablonas += L")";
+    return tld_sablonas;
+}
+
+wregex url_israisku_nustatymas(){
+    wstring tld_sablonas = tld_saraso_sudarymas();
+    wregex url_israiska(
+        LR"((https?://|www\.)[^\s()<>]+|([a-zA-Z0-9.-]+\.)" + tld_sablonas + LR"((/[^\s()<>]*|(?=[\s()<> \t]|$))))",
+        icase
+    );
+    return url_israiska;
+}
---
+++ url_funkcijos.h
@@ -0,0 +1,12 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <regex>
+
+using std::wstring;
+using std::wregex;
+
+wstring tld_saraso_sudarymas();
+wregex url_israisku_nustatymas();
+
